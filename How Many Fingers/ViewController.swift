1c307694b73942c311eda279a00a06fff86e591a
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-03-11 18:09:41 +0000

    Initial Commit

---
+++ How Many Fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by Fayimora Femi-Balogun on 11/03/2016.
+//  Copyright © 2016 Fayimora Femi-Balogun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
